48ebd04ed5dd09afaa1d2366b51464c9f6192965
ivykim-code <77889925+ivykim-code@users.noreply.github.com>
2021-03-24 19:57:02 -0400

    Update 2-Train Your Expert.swift

--- Code 2/3-Initialization/2-Train Your Expert.swift
+++ Code 2/3-Initialization/2-Train Your Expert.swift
@@ -1,1 +1,23 @@
+func moveForward2Times() {
+    expert.moveForward()
+    expert.moveForward()
+}
+
+func turnAround() {
+    expert.turnLeft()
+    expert.turnLeft()
+}
+func crossTheLineAndCollectGem() {
+    moveForward2Times()
+    expert.turnLockDown()
+    if expert.isOnGem{
+        expert.collectGem()
+    }
+}
+turnAround()
+for i in 1 ... 2 {
+    moveForward2Times()
+    expert.moveForward()
+    crossTheLineAndCollectGem()
+}
 
